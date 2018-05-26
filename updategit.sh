2e44c8800f0fbd18ad764daeb68ccc837b8f7d59
datamitra <datamitra1@gmail.com>
2018-05-26 14:20:55 +0530

    test

--- updategit.sh
+++ updategit.sh
@@ -2,8 +2,3 @@ git add *
 git commit -m "test"
 #git config credential.helper store
 git push
-
-
-# to set who is commiting
-#git config --global user.name "datamitra"
-#git config --global user.email "datamitra1@gmail.com"
